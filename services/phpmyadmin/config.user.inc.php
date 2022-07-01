3a6f850ec01193f2ad6666c2abbba2e54e993e70
郭晓真 <guoxiaozhen@inke.cn>
2022-07-01 16:31:14 +0800

    修改: 默认phpmyadmin设置

--- services/phpmyadmin/config.user.inc.php
+++ services/phpmyadmin/config.user.inc.php
@@ -17,3 +17,15 @@ $cfg['Servers'][$i]['verbose']   = 'no2';
 $cfg['Servers'][$i]['host']      = 'mysql5';
 */
 
+$cfg['blowfish_secret']='multiServerExample70518';
+
+$i = 0;
+
+$i++; // server 1 :
+$cfg['Servers'][$i]['verbose']   = 'docker-mysql';
+$cfg['Servers'][$i]['host']      = 'mysql';
+
+
+$i++; // server 2 :
+$cfg['Servers'][$i]['verbose']   = 'docker-mysql5';
+$cfg['Servers'][$i]['host']      = 'mysql5';
