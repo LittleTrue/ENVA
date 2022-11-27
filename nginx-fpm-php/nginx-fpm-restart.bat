b2c4f5bb4f05f4cdb8a82b3a5da1d89a060b22cc
LittleTrue <30282900+LittleTrue@users.noreply.github.com>
2022-11-27 22:24:29 +0800

    新增说明

--- nginx-fpm-php/nginx-fpm-restart.bat
+++ nginx-fpm-php/nginx-fpm-restart.bat
@@ -8,7 +8,7 @@ ipconfig /flushdns
 
 ECHO Reload PHP FastCGI...
 taskkill /F /IM php-cgi.exe > null
-%ENV_PATH%/nginx-fpm-php/RunHiddenConsole/RunHiddenConsole.exe %FPM_PATH%/php-cgi.exe -b 127.0.0.1:9000
+%ENV_PATH%/nginx-fpm-php/RunHiddenConsole.exe %FPM_PATH%\php-cgi.exe -b 127.0.0.1:9000
 
 ECHO Reload NGINX ...
 taskkill /F /IM  nginx.exe > null
