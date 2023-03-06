71519f3e28d55eabedccc6bdf706df669650c272
郭晓真 <guoxiaozhen@inke.cn>
2023-03-06 15:59:31 +0800

    add

--- nginx-fpm-php/nginx-fpm-start.bat
+++ nginx-fpm-php/nginx-fpm-start.bat
@@ -8,5 +8,7 @@ ipconfig /flushdns
 ECHO Starting PHP FastCGI...
 %ENV_PATH%/nginx-fpm-php/RunHiddenConsole.exe %FPM_PATH%\php-cgi.exe -b 127.0.0.1:9000
 
+ECHO %ENV_PATH%/nginx-fpm-php/RunHiddenConsole.exe %FPM_PATH%\php-cgi.exe -b 127.0.0.1:9000
+
 ECHO Starting NGINX ...
 cmd /k "cd /d %ENV_PATH%/nginx & start nginx & exit"
